bf5cd88ab4f507eecc9c93cce7899df80370bba4
Brandt Hughes <brandt@brandthughes.com>
2021-09-09 07:07:56 -0500

    complete restructuring, error checks, sponsor regexp fix
    Functions are now ordered in their order of operation to more easily navigate.
    
    A few global variables have been created to easily pull into some functions without passing through many functions.
    
    Error check function has been created to stop the process if a mismatch is found, and then a descriptive error message prompts the user to notify them exactly what went wrong.
    
    The sponsor regexp filter previously only returned the first word before a space, which wasn't intended. It now returns the entire string before a " (" which is intended.

--- COB-functions.js
+++ COB-functions.js
@@ -1,24 +1,153 @@
 // ░░░░░░░░░▓ GLOBAL VARIABLES
 let uploads;
+let pubNum;
+let adDiag;
+let epDiag;
+let rowDiag;
+let errorMessage;
+let errorFailure;
 
 
 // ░░░░░░░░░▓ FUNCTION THAT GETS CALLED FROM THE MENU ITEM
-function closeOutEpisode() {
+function closeOutButton() {
 	
 	const cell = ss.getActiveSheet().getCurrentCell();
 	const epLabel = cell.getValue();
 	const thisRow = cell.getRow();
+	const adLabel = adInput(thisRow);
 	
-	tab2.getRange("B" + thisRow + ":O" + thisRow).setBackground("#d9d9d9");
+	adDiag = adLabel[0];
+	epDiag = epLabel;
+	rowDiag = thisRow;
+	
 	getYoutubeData();
-	submitDataTab2(thisRow);
-	submitDataTab1(epLabel);
-	// formatDataTab1();
+	findMatches(epLabel, adLabel);
 }
 
+// ░░░░░░░░░▓ FILTERS THE SPONSOR DATA TO REMOVE EVERYTHING AFTER " ("
+function adInput(row){
+	if (tab2.getRange("K" + row).getValue().match(/.*?(?= \()/) == null){
+		return tab2.getRange("K" + row).getValue().match(/.*/);
+	} else {
+		return tab2.getRange("K" + row).getValue().match(/.*?(?= \()/);
+	};
+}
+
+// ░░░░░░░░░▓ PULLS YOUTUBE DATA, STORES IT IN GLOBAL VARIABLE "UPLOADS"
+function getYoutubeData(){
+	
+	let results = YouTube.Channels.list('contentDetails', {
+		id: "UC42VsoDtra5hMiXZSsD6eGg"
+	});
+
+	for (var i = 0; i < results.items.length; i++) {
+		var item = results.items[i];
+		var playlistId = item.contentDetails.relatedPlaylists.uploads;
+		var playlistResponse = YouTube.PlaylistItems.list('snippet', {
+			playlistId: playlistId,
+			maxResults: 5,
+		});
+	}
+
+	uploads = playlistResponse;
+}
+
+// ░░░░░░░░░▓ COMPARE EP AND AD LABELS TO DATA FROM THE PRODUCTION TAB
+function findMatches(ep, ad){
+	
+	const lastRow = tab1.getLastRow();
+	const labelRange = tab1.getRange((lastRow - 39), 12, 40, 1);
+	var matchedEpRows = [];
+	var matchedAdRows = [];
+	
+	for ( i = 0; i < 40; i++) {
+		// SENDS THE ROW NUMBER OF MATCHING *EPISODE* ENTRIES FROM TAB 1 TO matchedEpRows ARRAY
+		if (labelRange.getValues()[i].toString() == ep) {
+			matchedEpRows.push(lastRow - (39 - Number([i]))); };
+		
+		// SENDS THE ROW NUMBER OF MATCHING *AD* ENTRIES FROM TAB 1 TO matchedAdRows ARRAY
+		if (ad != null) {
+			if (new RegExp(ad[0],'i').test(labelRange.getValues()[i]) == true && labelRange.getBackgrounds()[i] == "#90eba6") {
+				matchedAdRows.push(Number(lastRow - (39 - Number([i])))) };
+		};
+	};
+
+	errorCheck(matchedEpRows, matchedAdRows);
+}
+
+// ░░░░░░░░░▓ IF A MATCH ISN'T FOUND, RUN AN ERROR MESSAGE AND END PROCESS
+function errorCheck(eps, ads){
+	
+	if (eps[0] == null){
+		errorMessage = "Your episode did not close successfully.";
+		errorFailure = epDiag;
+		
+		const htmlForModal = HtmlService.createTemplateFromFile("COB-error");
+		const htmlOutput = htmlForModal.evaluate();
+			htmlOutput.setWidth(430);
+			htmlOutput.setHeight(90);
+		const ui = SpreadsheetApp.getUi();
+			ui.showModalDialog(htmlOutput, "Episode Error");
+	} else {
+		submitDataTab2(rowDiag);
+		closeOutEp(eps, ads);
+		
+		if (ads[0] == null){
+			errorMessage = "Your episode closed successfully, but your ad didn't.";
+			errorFailure = adDiag;
+			
+			const htmlForModal = HtmlService.createTemplateFromFile("COB-error");
+			const htmlOutput = htmlForModal.evaluate();
+				htmlOutput.setWidth(430);
+				htmlOutput.setHeight(90);
+			const ui = SpreadsheetApp.getUi();
+				ui.showModalDialog(htmlOutput, "Sponsor Error");
+		} else {
+			closeOutAd(ads);
+		}
+	}
+}
+
+// ░░░░░░░░░▓ EACH MATCHED EP ENTRY | SUBMITS DATA, FORMATS DATA, COLORS ROW, HIDES ROW
+function closeOutEp(eps, ads){
+	eps.forEach(function(rowNum){
+		if (tab1.getRange("M" + rowNum).getValue() > 1){
+			tab1.getRange(rowNum + ":" + rowNum).setBackground("#b7b7b7");
+		} else {
+			tab1.getRange(rowNum + ":" + rowNum).setBackground("#d9d9d9"); };
+
+		tab1.getRange("O" + rowNum).setValue(pubNum);
+		tab1.getRange("P" + rowNum).setValue(new Date(uploads.items[0].snippet.publishedAt))
+			.setNumberFormat("yyyy-mm-dd");
+		tab1.getRange("Q" + rowNum).setValue("https://youtu.be/" + uploads.items[0].snippet.resourceId.videoId)
+			.setHorizontalAlignment("right");
+		tab1.getRange("R" + rowNum).setValue(ads[0]);
+		tab1.hideRows(rowNum);
+	});
+}
+
+// ░░░░░░░░░▓ OLDEST MATCHED AD ENTRY | SUBMITS DATA, FORMATS DATA, COLORS ROW, HIDES ROW
+function closeOutAd(ads){
+	if (ads != null){
+		let oldestAd;
+		
+		oldestAd = Math.min(...ads);
+		
+		tab1.getRange(oldestAd + ":" + oldestAd).setBackground("#b7b7b7");
+		tab1.getRange("O" + oldestAd).setValue(pubNum);
+		tab1.getRange("P" + oldestAd).setValue(new Date(uploads.items[0].snippet.publishedAt))
+			.setNumberFormat("yyyy-mm-dd");
+		tab1.getRange("Q" + oldestAd).setValue("https://youtu.be/" + uploads.items[0].snippet.resourceId.videoId)
+			.setHorizontalAlignment("right");
+		tab1.hideRows(oldestAd);
+	}
+}
 
 // ░░░░░░░░░▓ SUBMITS AND MODIFIES DATA IN TAB 2
 function submitDataTab2(row){
+	
+	// sets background color for the entire row
+	tab2.getRange("B" + row + ":O" + row).setBackground("#d9d9d9");
 	
 	// fills out release number
 	tab2.getRange("B" + row)
@@ -47,24 +176,6 @@ function submitDataTab2(row){
 		.setVerticalAlignment("middle");
 }
 
-function submitDataTab1(label){
-	
-	let lastRow = tab1.getLastRow();
-	const labelRange = tab1.getRange((lastRow - 40), 12, 40, 1);
-	var matchingRows = [];
-		
-	for ( i = 0; i < 40; i++){
-		if (labelRange.getValues()[i].toString() == label) {
-			matchingRows.push(lastRow - (40 - Number([i])));
-		};
-	};
-	
-	matchingRows.forEach(function(el){
-		tab1.getRange(el + ":" + el).setBackground("#d9d9d9");
-		});
-}
-
-
 // ░░░░░░░░░▓ DETERMINES PUBLISH NUMBER
 function determinePubNum(row){
 	
@@ -76,25 +187,6 @@ function determinePubNum(row){
 		{ recentPubNums.push(Number(rangePubNums.getValues()[i])); };
 	};
 
-	return Math.max(...recentPubNums) + 1;
-}
-
-
-// ░░░░░░░░░▓ PULLS YOUTUBE DATA, STORES IT IN GLOBAL VARIABLE "UPLOADS"
-function getYoutubeData(){
-	
-	let results = YouTube.Channels.list('contentDetails', {
-		id: "UC42VsoDtra5hMiXZSsD6eGg"
-	});
-
-	for (var i = 0; i < results.items.length; i++) {
-		var item = results.items[i];
-		var playlistId = item.contentDetails.relatedPlaylists.uploads;
-		var playlistResponse = YouTube.PlaylistItems.list('snippet', {
-			playlistId: playlistId,
-			maxResults: 5,
-		});
-	}
-
-	uploads = playlistResponse;
+	pubNum = Math.max(...recentPubNums) + 1;
+	return pubNum;
 }
