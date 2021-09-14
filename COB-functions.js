6d3f6b49489986bdf9ea2f4380378519407b5923
Brandt Hughes <brandt@brandthughes.com>
2021-09-13 20:53:44 -0500

    set corrective suggestions to run by default and give the cob its own data retrieval function
    Corrective suggestion function has been moved up and now runs by default when looking at the user input. This circumvents a nested candidate issue, and ultimately requires less user input while offering greater accuracy. It also runs faster in cases of user input typos at little detriment to perfect or null user input. It has also been bolstered with better matching techniques, a consecutive match bonus, and code that generally acts more as it was intended.
    
    runWithSuggestion has been removed since suggestions now always run instead of when prompted.
    
    Some of findMatches has been simplified, and this is now where suggestMatch is called if given ad input.

--- COB-functions.js
+++ COB-functions.js
@@ -7,7 +7,7 @@ let rowDiag;
 let errorMessage;
 let errorFailure;
 let bestAdMatch;
-let useMatch = false;
+let adCorrectiveConf;
 
 
 // ░░░░░░░░░▓ FUNCTION THAT GETS CALLED FROM THE MENU ITEM
@@ -21,6 +21,7 @@ function closeOutButton() {
 	adDiag = adLabel[0];
 	isEpMissing(epLabel);
 	getYoutubeData();
+	getSheetDataCoB(newRow - 70);
 	findMatches(epLabel, adLabel);
 }
 
@@ -70,32 +71,112 @@ function getYoutubeData(){
 	uploads = playlistResponse;
 }
 
+// ░░░░░░░░░▓ READS DATA FROM THE SPREADSHEET WHEN THE SIDEBAR LOADS,
+// ░░░░░░░░░▓ ALL RELEVANT DATA GETS PASSED TO THEIR RESPECTIVE ARRAYS IN OBJECT "dataArray"
+function getSheetDataCoB(offset){
+	const labelRange = tab1.getRange(offset,12,newRow - (offset-1),1);
+	const locationWideRange = tab1.getRange(offset,19,newRow - (offset-1),1);
+	const locationNarrowRange = tab1.getRange(offset,20,newRow - (offset-1),1);
+
+		for ( i = 0; i < ((newRow + 1) - offset); i++){
+			if (labelRange.getBackgrounds()[i] == "#ffff00") {
+				dataArray.unfinished.push(labelRange.getValues()[i])
+			} else if (locationWideRange.getValues()[i] != "") {
+				dataArray.locationsWide.push(locationWideRange.getValues()[i]);
+				dataArray.locationsNarrow.push(locationNarrowRange.getValues()[i]);
+			} else if (labelRange.getValues()[i] != "") {
+				dataArray.allLabels.push(labelRange.getValues()[i])
+			};
+			
+			if (labelRange.getBackgrounds()[i] == "#90eba6") {
+				dataArray.recSponsors.push(labelRange.getValues()[i]);
+			};
+		};
+		
+	dataArray.allSponsors = labelRange.getValues().filter(value => /^ad: /i.test(value));
+
+	return dataArray;
+}
+
 // ░░░░░░░░░▓ COMPARE EP AND AD LABELS TO DATA FROM THE PRODUCTION TAB
 function findMatches(ep, ad){
 	
 	const lastRow = tab1.getLastRow();
 	const labelRange = tab1.getRange((lastRow - 39), 12, 40, 1);
-  var matchedEpRows = [];
-  var matchedAdRows = [];
+	var matchedEpRows = [];
+	var matchedAdRows = [];
+	
+		// SENDS THE ROW NUMBER OF MATCHING *AD* ENTRIES FROM TAB 1 TO matchedAdRows ARRAY
+	if (ad != "") {
+		suggestMatch();
+	};
 	
 	for ( i = 0; i < 40; i++) {
+		let rollingCell = labelRange.getValues()[i].toString();
 		// SENDS THE ROW NUMBER OF MATCHING *EPISODE* ENTRIES FROM TAB 1 TO matchedEpRows ARRAY
-		if (labelRange.getValues()[i].toString() == ep) {
-			matchedEpRows.push(lastRow - (39 - Number([i]))); };
-		
-		// SENDS THE ROW NUMBER OF MATCHING *AD* ENTRIES FROM TAB 1 TO matchedAdRows ARRAY
-		if (ad != null) {
-			if (new RegExp(ad[0],'i').test(labelRange.getValues()[i]) == true && labelRange.getBackgrounds()[i] == "#90eba6") {
-				matchedAdRows.push(Number(lastRow - (39 - Number([i])))) };
-		};
+		if (rollingCell == ep)
+			{ matchedEpRows.push(lastRow - (39 - Number([i]))) };
+		if (rollingCell == "ad: " + bestAdMatch && labelRange.getBackgrounds()[i] == "#90eba6")
+			{ matchedAdRows.push(Number(lastRow - (39 - Number([i])))) };
 	};
 
 	errorCheck(matchedAdRows, matchedEpRows, ep);
 }
 
+// ░░░░░░░░░▓ SUGGESTS THE NEAREST CANDIDATE TO CORRECT A TYPO
+function suggestMatch(){
+	
+	let adStr = adDiag.toLocaleLowerCase();											// user input set to all lower case
+	let adArr = [].concat.apply([], dataArray.recSponsors);							// a flattened array of recorded sponsors
+	let filteredCandidates = [];													// a list of each candidate's confidence rating
+	let bestMatch;																	// the lowest number from filteredCandidates
+					
+	adArr.forEach(function(adCandidate) {											// run through each ad candidate
+		let candidateSearchStr = adCandidate.slice(4).toLocaleLowerCase();			// removes "ad: " from the beginning of the candidate
+		let canLength = candidateSearchStr.length;									// length of candidate string before filtering
+		let deviations = 0;															// how many letters are input but not matched
+		let consBonus = 0;															// cumulative consecutive matches
+		let consCount = -1;															// variable consecutive match counter
+		let adLength = adStr.length;												// length of user input as a number
+							
+		for ( i = 0; i < adLength; i++) {											// runs through each letter of the user input
+				
+			if (candidateSearchStr.includes(adStr[i]) == true) {				// if the letter exists in the candidate, then...
+				let x = candidateSearchStr.indexOf(adStr[i]);						// where the letter is first found
+				let str1 = candidateSearchStr.slice(0, x);							// cut everything before the letter
+				let str2 = candidateSearchStr.slice(x + 1);							// cut everything after the letter
+				candidateSearchStr = str1 += str2;									// combine the strings to remove the letter
+				consCount++;	
+					
+			} else {															// but if it can't be found in the candidate...
+				deviations++;														// ... then it increases total length
+				if (consCount >= 1){ consBonus = consBonus + consCount };			// send consecutive bonus if it's built up
+				consCount = -1;														// reset consecutive bonus	
+			};
+			
+			if ([i] == (adLength - 1) && (consCount == -1))	{consBonus = consBonus + 0}
+			else if ([i] == (adLength - 1) && (consCount != -1)) {consBonus = consBonus + consCount};
+		};
+
+		let roundedLength = Math.round(100*											// rounds to hundredth place
+			(candidateSearchStr.length + deviations)*								// adds mismatches from the user input to the total length
+			((canLength - consBonus) / canLength))									// applies a bonus based on consecutive matches
+			/100;																	// completes the rounding
+		let proportionalLength = Math.round(										// rounds to nearest integer
+			(1 - (roundedLength / (canLength + candidateSearchStr.length + deviations)))
+			* 100);																	// final length as a percentage of start length + mismatches
+		
+		filteredCandidates.push(proportionalLength);								// send filtered candidate string to list
+	});
+	
+	bestMatch = Math.max(...filteredCandidates);									// find the candidate with the lowest number
+	let bestMatchLabel = adArr[filteredCandidates.indexOf(bestMatch)].slice(4);		// return the corresponding title without "ad: "
+	bestAdMatch = bestMatchLabel;													// global version of the above
+	adCorrectiveConf = filteredCandidates[filteredCandidates.indexOf(bestMatch)];	// sets adCorrectiveConf to the confidence rating
+}
+
 // ░░░░░░░░░▓ IF A MATCH ISN'T FOUND, RUN AN ERROR MESSAGE AND END PROCESS
 function errorCheck(ads, eps, epLabel){
-	
 	if (eps[0] == null){
 		errorMessage = "Your episode did not close successfully.";
 		errorFailure = epDiag;
@@ -104,58 +185,19 @@ function errorCheck(ads, eps, epLabel){
 		const htmlOutput = htmlForModal.evaluate();
 			htmlOutput.setWidth(410);
 			htmlOutput.setHeight(130);
-		const ui = SpreadsheetApp.getUi();
-			ui.showModalDialog(htmlOutput, "Episode Error");
-	} else {		
-		if (ads[0] == null && useMatch == false) {
-			suggestMatch();
-			errorMessage = "Your episode found a match, but your ad didn't.";
-			errorFailure = adDiag;
-			
+			SpreadsheetApp.getUi().showModalDialog(htmlOutput, "Episode Error");
+	} else {
+		if (adCorrectiveConf != 100 && adDiag != "") {				// if the user input ad anchor has text but isn't a perfect match
+			commitClose(eps, ads);
 			const htmlForModal = HtmlService.createTemplateFromFile("COB-ad-error");
 			const htmlOutput = htmlForModal.evaluate();
-				htmlOutput.setWidth(410);
-				htmlOutput.setHeight(170);
-			const ui = SpreadsheetApp.getUi();
-				ui.showModalDialog(htmlOutput, "Sponsor Error");
-		} else if (ads[0] == null && useMatch == true) {
-			suggestMatch();
-			findMatches(epLabel, bestAdMatch);
+				htmlOutput.setWidth(314);
+				htmlOutput.setHeight(250);
+			SpreadsheetApp.getUi().showModalDialog(htmlOutput, "Corrective Assumption");
 		} else {
 			commitClose(eps, ads);
 		}
 	}
-}
-
-// ░░░░░░░░░▓ SUGGESTS THE NEAREST CANDIDATE TO CORRECT A TYPO
-function suggestMatch(){
-	
-	// dataArray.sponsors is the array of ads
-	getSpreadsheetData();
-	
-	let adStr = adDiag.toLocaleLowerCase();		// the lowercased sponsor input from user
-	let adArr = [].concat.apply([], dataArray.sponsors);
-	let filteredCandidates = [];
-	let bestMatch;
-
-	adArr.forEach(function(adCandidate) {
-		var filteredName = adCandidate.slice(4);
-		let adLength = adStr.length;
-		
-		for ( i = 0; i < adLength; i++) {
-			if (adCandidate.toLocaleLowerCase().includes(adStr[i]) == true) {
-				filteredName = filteredName.replace(new RegExp(adStr[i],"i"),'');
-			} else {
-				filteredName += adStr[i];
-			};
-		};
-		
-		filteredCandidates.push(filteredName.length);
-	});
-	
-	bestMatch = Math.min(...filteredCandidates);
-	let bestMatchLabel = adArr[filteredCandidates.indexOf(bestMatch)].slice(4);
-	bestAdMatch = bestMatchLabel;
 }
 
 // ░░░░░░░░░▓ RUN ALL OF THE FUNCTIONS THAT ACTUALLY CHANGE CELLS
@@ -186,7 +228,7 @@ function closeOutEp(eps, ads){
 
 // ░░░░░░░░░▓ OLDEST MATCHED AD ENTRY | SUBMITS DATA, FORMATS DATA, COLORS ROW, HIDES ROW
 function closeOutAd(eps, ads){
-	if (ads != null){
+	if (ads != ""){
 		let firstAdRow;
 		
 		firstAdRow = Math.min(...ads);
@@ -251,13 +293,4 @@ function determinePubNum(row){
 
 	pubNum = Math.max(...recentPubNums) + 1;
 	return pubNum;
-}
-
-// ░░░░░░░░░▓ LOOPS BACK THROUGH CLOSING OUT BUT NOW DEFAULTING TO USE SUGGESTED MATCH
-function runWithSuggestion(){
-	/*	after navigating the modal, all data gets lost,
-		so we have to run through everything again
-		but this time default to using the ad match */
-	useMatch = true;
-	closeOutButton();
 }
